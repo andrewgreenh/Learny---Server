e54c3d583b7e26476e7d2ff08d2882348202dd06
abefort <s5725119@mail.zih.tu-dresden.de>
2015-06-05 11:51:45 +0200

    ArchievmentController angelegt

---
+++ src/main/java/de/learny/controller/AchievementController.java
@@ -0,0 +1,10 @@
+package de.learny.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/achievements")
+public class AchievementController {
+
+}
