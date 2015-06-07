453b2c03a3153933e60eed5eccea80558afe0276
Andreas Roth <art_github@rtline.de>
2015-06-07 12:18:24 +0200

    Fügt die neuen Methoden zum Verändern der Relationsattribute hinzu

--- src/main/java/de/learny/domain/Account.java
+++ src/main/java/de/learny/domain/Account.java
@@ -1,8 +1,11 @@
 package de.learny.domain;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +15,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 @Entity
@@ -21,6 +25,7 @@ public class Account {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
+	@Column(unique=true)
 	private String accountName;
 	
 	private String password, surname, lastname, email, avatarUri;
@@ -28,7 +33,7 @@ public class Account {
 	@ManyToOne
 	private Role role;
 
-	@OneToMany(mappedBy = "account")
+	@OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
 	private Set<TestScore> testScores = new HashSet<TestScore>();
 
 	@ManyToMany
@@ -54,7 +59,8 @@ public class Account {
 	public String getPassword() {
 		return password;
 	}
-
+	
+	@JsonProperty
 	public void setPassword(String password) {
 		this.password = password;
 	}
@@ -114,18 +120,58 @@ public class Account {
 
 	@JsonIgnore
 	public Set<Subject> getAdministratedSubjects() {
-		return administratedSubjects;
+		return Collections.unmodifiableSet(administratedSubjects);
+	}
+	
+	public boolean addAdministratedSubject(Subject subject) {
+		this.administratedSubjects.add(subject);
+		if (!subject.getAccountsInCharge().contains(this)) {
+			subject.addAccountInCharge(this);
+		}
+		return true;
+	}
+	
+	public boolean removeAdministratedSubject(Subject subject) {
+		this.administratedSubjects.remove(subject);
+		if(subject.getAccountsInCharge().contains(this)) {
+			subject.removeAccountInCharge(this);
+		}
+		return true;
 	}
 
 	public Set<Achievement> getAchievements() {
-		return achievements;
+		return Collections.unmodifiableSet(achievements);
+	}
+	
+	public boolean addAchievement(Achievement achievement) {
+		return this.achievements.add(achievement);
+	}
+	
+	public boolean removeAchievement(Achievement achievement) {
+		return this.achievements.remove(achievement);
 	}
 
 	@JsonIgnore
 	public Set<Subject> getJoinedSubjects() {
-		return joinedSubjects;
+		return Collections.unmodifiableSet(joinedSubjects);
 	}
 
+	public boolean addJoinedSubject(Subject subject) {
+		this.joinedSubjects.add(subject);
+		if(!subject.getParticipants().contains(this)) {
+			subject.addParticipant(this);
+		}
+		return true;
+	}
+	
+	public boolean removeJoinedSubject(Subject subject) {
+		this.joinedSubjects.remove(subject);
+		if(subject.getParticipants().contains(this)) {
+			subject.removeParticipant(this);
+		}
+		return true;
+	}
+	
 	
 
 }
--- src/main/java/de/learny/domain/Subject.java
+++ src/main/java/de/learny/domain/Subject.java
@@ -1,5 +1,6 @@
 package de.learny.domain;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -22,13 +23,13 @@ public class Subject {
 
 	private String name, description;
 
-	@ManyToMany(mappedBy = "administratedSubjects")
+	@ManyToMany(mappedBy = "administratedSubjects", cascade = CascadeType.ALL)
 	private Set<Account> accountsInCharge = new HashSet<Account>();
 
-	@ManyToMany(mappedBy = "joinedSubjects")
+	@ManyToMany(mappedBy = "joinedSubjects", cascade = CascadeType.ALL)
 	private Set<Account> participants = new HashSet<Account>();
 
-	@OneToMany(mappedBy = "subject")
+	@OneToMany(mappedBy = "subject", cascade = CascadeType.ALL, orphanRemoval = true)
 	private Set<Test> tests = new HashSet<Test>();
 
 	public Subject(String name) {
@@ -60,17 +61,65 @@ public class Subject {
 	}
 
 	public Set<Account> getAccountsInCharge() {
-		return accountsInCharge;
+		return Collections.unmodifiableSet(accountsInCharge);
+	}
+	
+	public boolean addAccountInCharge(Account account) {
+		this.accountsInCharge.add(account);
+		if(!account.getAdministratedSubjects().contains(this)) {
+			account.addAdministratedSubject(this);
+		}
+		return true;
+	}
+	
+	public boolean removeAccountInCharge(Account account) {
+		this.accountsInCharge.remove(account);
+		if(account.getAdministratedSubjects().contains(account)) {
+			account.removeAdministratedSubject(this);
+		}
+		return true;
 	}
 
 	@JsonIgnore
 	public Set<Account> getParticipants() {
-		return participants;
+		return Collections.unmodifiableSet(participants);
+	}
+	
+	public boolean addParticipant(Account account) {
+		this.participants.add(account);
+		if(!account.getJoinedSubjects().contains(this)) {
+			account.addJoinedSubject(this);
+		}
+		return true;
+	}
+	
+	public boolean removeParticipant(Account account) {
+		this.participants.remove(account);
+		if(account.getJoinedSubjects().contains(this)) {
+			account.removeJoinedSubject(this);
+		}
+		return true;
 	}
 
 	@JsonIgnore
 	public Set<Test> getTests() {
-		return tests;
+		return Collections.unmodifiableSet(tests);
+	}
+	
+	public boolean addTest(Test test) {
+		this.tests.add(test);
+		if(test.getSubject() != this) {
+			test.setSubject(this);
+		}
+		return true;
+	}
+	
+	public boolean removeTest(Test test) {
+		this.tests.remove(test);
+		if(test.getSubject() == this) {
+			test.setSubject(null);
+		}
+		return true;
 	}
 
 }
