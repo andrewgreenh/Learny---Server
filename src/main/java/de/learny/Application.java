c90008368d56d56c22356ebed0772281f603e631
abefort <s5725119@mail.zih.tu-dresden.de>
2015-06-15 12:43:06 +0200

    Merge Conflict angepasst

--- src/main/java/de/learny/Application.java
+++ src/main/java/de/learny/Application.java
@@ -58,69 +58,75 @@ public class Application extends SpringBootServletInitializer implements Command
 	@Autowired
 	AnswerRepository answerRepo;
 	
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	@Autowired
+	private Environment environment;
 
-    @Override
-    public void run(String... strings) throws Exception {
-    	Role adminRole = new Role("admin");
-    	Role dozentRole = new Role("dozent");
-    	Role userRole = new Role("user");    	
-    	
-    	roleRepo.save(adminRole);
-    	roleRepo.save(dozentRole);
-    	roleRepo.save(userRole);
-    	
-    	Subject sub1 = new Subject("Fach1");
-    	Subject sub2 = new Subject("Fach2");
-    	subjectRepo.save(sub1);
-    	subjectRepo.save(sub2);
-    	Test test1 = new Test("test1", sub1);
-    	Test test3 = new Test("test3", sub1);
-    	Test test2 = new Test("test2", sub2);
-    	testRepo.save(test1);
-    	testRepo.save(test3);
-    	testRepo.save(test2);
-    	
-    	Question quest1 = new Question("frage1", test1);
-    	questionRepo.save(quest1);
-    	Answer answer1 = new Answer("antwort1", quest1, true);
-    	Answer answer2 = new Answer("antwort2", quest1, false);
-    	answerRepo.save(answer1);
-    	answerRepo.save(answer2);
-    	
-    	Account student = new Account("student", passwordGenerator.hashPassword("student"));
-    	student.setSurname("Conrad");
-    	student.setLastname("Reuter");
-    	student.setEmail("a@bd.de");
-    	
-    	Account admin = new Account("admin", passwordGenerator.hashPassword("admin"));
-    	admin.setSurname("Andreas");
-    	admin.setLastname("Roth");
-    	admin.setEmail("a@bd.de");
-    	
-    	Account dozent = new Account("dozent", passwordGenerator.hashPassword("dozent"));
-    	dozent.setSurname("Martin");
-    	dozent.setLastname("Burwitz");
-    	dozent.setEmail("a@bd.de");
-    	
-    	student.addRole(userRole);
-    	admin.addRole(adminRole);
-    	dozent.addRole(dozentRole);
-    	
-    	accountRepo.save(admin);
-    	accountRepo.save(student);
-    	accountRepo.save(dozent);
-    	
-    	sub1.addAccountInCharge(dozent);
-    	subjectRepo.save(sub1);
-    	student.addJoinedSubject(sub1);
-    	accountRepo.save(student);
-    }
-    
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		String[] envVars = environment.getActiveProfiles();
+		if (!(envVars.length > 0 && envVars[0].equals("demo"))) {
+			Role adminRole = new Role("admin");
+			Role dozentRole = new Role("dozent");
+			Role userRole = new Role("user");
+
+			roleRepo.save(adminRole);
+			roleRepo.save(dozentRole);
+			roleRepo.save(userRole);
+
+			Subject sub1 = new Subject("Fach1");
+			Subject sub2 = new Subject("Fach2");
+			subjectRepo.save(sub1);
+			subjectRepo.save(sub2);
+			Test test1 = new Test("test1", sub1);
+			Test test3 = new Test("test3", sub1);
+			Test test2 = new Test("test2", sub2);
+			testRepo.save(test1);
+			testRepo.save(test3);
+			testRepo.save(test2);
+
+			Question quest1 = new Question("frage1", test1);
+			questionRepo.save(quest1);
+			Answer answer1 = new Answer("antwort1", quest1, true);
+	    	Answer answer2 = new Answer("antwort2", quest1, false);
+			answerRepo.save(answer1);
+			answerRepo.save(answer2);
+
+			Account student = new Account("student", passwordGenerator.hashPassword("student"));
+			student.setSurname("Conrad");
+			student.setLastname("Reuter");
+			student.setEmail("a@bd.de");
+
+			Account admin = new Account("admin", passwordGenerator.hashPassword("admin"));
+			admin.setSurname("Andreas");
+			admin.setLastname("Roth");
+			admin.setEmail("a@bd.de");
+
+			Account dozent = new Account("dozent", passwordGenerator.hashPassword("dozent"));
+			dozent.setSurname("Martin");
+			dozent.setLastname("Burwitz");
+			dozent.setEmail("a@bd.de");
+
+			student.addRole(userRole);
+			admin.addRole(adminRole);
+			dozent.addRole(dozentRole);
+
+			accountRepo.save(admin);
+			accountRepo.save(student);
+			accountRepo.save(dozent);
+
+			sub1.addAccountInCharge(dozent);
+			subjectRepo.save(sub1);
+			student.addJoinedSubject(sub1);
+			accountRepo.save(student);
+		}
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 }
