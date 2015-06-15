5850451123a54c53b97ca3f69c2fdb50f71a4f71
abefort <s5725119@mail.zih.tu-dresden.de>
2015-06-15 14:41:32 +0200

    Unnötige imports gelöscht

--- src/main/java/de/learny/domain/TestScore.java
+++ src/main/java/de/learny/domain/TestScore.java
@@ -10,9 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class TestScore {
