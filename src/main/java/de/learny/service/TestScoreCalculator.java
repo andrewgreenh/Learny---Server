1ed54c9de7be39f5d00a1e18e5b76f090570d133
abefort <s5725119@mail.zih.tu-dresden.de>
2015-06-29 14:06:47 +0200

    TestScoreCalculator implementiert

--- src/main/java/de/learny/service/TestScoreCalculator.java
+++ src/main/java/de/learny/service/TestScoreCalculator.java
@@ -1,5 +1,42 @@
 package de.learny.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import de.learny.dataaccess.AnswerRepository;
+import de.learny.domain.Answer;
+import de.learny.domain.TestScore;
+
+@Service
 public class TestScoreCalculator {
+
+	private TestScore testScore;	
+	
+	public Map<String, Integer> calculateRightAnswers() {
+		Map<String,Integer> result = new HashMap<String,Integer>();
+		int answerCount = 0;
+		int errorCount = 0;
+		for(Answer checkedAnswer: testScore.getCheckedAnswers()){
+			if(!checkedAnswer.isCorrect()) {
+				errorCount++;
+			} 
+			answerCount++;
+		}
+		result.put("Fehler", errorCount);
+		result.put("Antworten abgegeben", answerCount);
+		return result;
+		
+	}
+
+	public TestScore getTestScore() {
+		return testScore;
+	}
+
+	public void setTestScore(TestScore testScore) {
+		this.testScore = testScore;
+	}
 
 }
