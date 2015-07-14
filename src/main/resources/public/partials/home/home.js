e11b0094eda81b13e1b9acfafedd295f819bb55a
Andreas Roth <art_github@rtline.de>
2015-07-14 11:51:58 +0200

    Accountfinder wirft keinen fehler mehr, da jetzt auf undefined beim suchstring geprüft wird

--- src/main/resources/public/partials/home/home.js
+++ src/main/resources/public/partials/home/home.js
@@ -1,12 +1,17 @@
-angular.module('learny').controller('homeController',
-        [ '$scope', 'subjects', 'adminSubjects', function($scope, subjects, adminSubjects) {
-            
-            $scope.subjects = subjects.data;
-            $scope.adminSubjects = adminSubjects.data;
-            $scope.showAdminSucjects = false;
-            
-            if($scope.adminSubjects.length > 0) {
-                $scope.showAdminSubjects = true;
-            }
-        
-        } ]);
+angular.module('learny').controller(
+        'homeController',
+        [ '$scope', 'subjects', 'adminSubjects', 'subjectsAdministratedByUser',
+                function($scope, subjects, adminSubjects, subjectsAdministratedByUser) {
+
+                    $scope.subjects = subjects.data;
+                    $scope.adminSubjects = adminSubjects.data;
+                    $scope.showAdminSucjects = false;
+                    $scope.showSucjects = false;
+
+                    if ($scope.adminSubjects.length > 0) {
+                        $scope.showAdminSubjects = true;
+                    }
+                    if ($scope.subjects.length > 0) {
+                        $scope.showSubjects = true;
+                    }
+                } ]);
